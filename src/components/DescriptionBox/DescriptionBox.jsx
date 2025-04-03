82c8f8a6acf82af075d15a18a8963a55442ff565
koshik_das_38 <koshikdas245@gmail.com>
2025-04-04 00:17:17 +0600

    Description part edited

--- src/components/DescriptionBox/DescriptionBox.jsx
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -12,10 +12,10 @@ const DescriptionBox = () => {
       <div className="descriptionbox-description">
         <p>An ecommerce website is an online store where customers can find products, browse offerings, and place purchases online. It facilitates the transaction between a buyer and seller.
 
-A digital storefront can serve as the virtual equivalent of the product shelves, sales staff, and cash register of a physical shop. Other components of an online store might include product listings, product categories, and customer reviews.</p>
-     <p>
-        E-commerce website typically display products or services along with deatailed description, images, prices and any avaliable varoations (e.g.,sizes,colors).Each product usually has its own dedicated page with relevant information
-     </p>
+          A digital storefront can serve as the virtual equivalent of the product shelves, sales staff, and cash register of a physical shop. Other components of an online store might include product listings, product categories, and customer reviews.</p>
+        <p>
+          E-commerce website typically display products or services along with deatailed description, images, prices and any avaliable varoations (e.g.,sizes,colors).Each product usually has its own dedicated page with relevant information
+        </p>
       </div>
     </div>
   )
